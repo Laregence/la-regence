66e235313e90ff19912aaf75cafed6ac543a0ec3
la-regence <laregence.mdm@gmail.com>
2026-01-03 08:42:43 +0100

    maj

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://laregencemdm.fr/', // ton domaine
+  site: 'https://laregencemdm.fr', // ⚠️ sans slash final (meilleure pratique)
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwind()],
   },
